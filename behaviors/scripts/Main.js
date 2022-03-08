0ec46f082028de35f0ab6e488037aecee97006b4
ANightDazingZoroark <56536620+ANightDazingZoroark@users.noreply.github.com>
2022-03-08 10:46:31 +0800

    almost done yay

--- behaviors/scripts/Main.js
+++ behaviors/scripts/Main.js
@@ -1,3 +1,8 @@
 import "scripts/BileLauncherAmmunition.js";
 import "scripts/HeadcrabLauncherAmmunition.js";
 import "scripts/SonicCannonAmmunition.js";
+import { world } from "mojang-minecraft"
+
+world.events.tick.subscribe((ev) => {
+    world.getDimension('overworld').runCommand(`event entity @a borgy:remove_warning`);
+});
